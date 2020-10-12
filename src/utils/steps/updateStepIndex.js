fcf324a698081b35cf263c3642855f66ed887482
Lance Tipton <lancetipton04@gmail.com>
2020-10-12 09:26:03 -0700

    Updates   * Seemed to have fixed the input value space issues, but needs more tests

--- src/utils/steps/updateStepIndex.js
+++ src/utils/steps/updateStepIndex.js
@@ -1,13 +1,9 @@
 import { devLog } from '../devLog'
 import { addQuotes } from '../helpers/addQuotes'
 
-const spaceHolder = `__PARAM_SPACE__`
 
 const checkInputValue = (value, current, param) => {
-  const updated = param?.type !== 'string' || current.indexOf('"') !== 0 ? value : addQuotes(value)
-
-  // Replace any spaces, so our step split does not get messed up
-  return updated.replace(/\s/g, spaceHolder)
+  return param?.type !== 'string' || current.indexOf('"') !== 0 ? value : addQuotes(value)
 }
 
 export const updateStepIndex = (step, value, row, param) => {
@@ -16,26 +12,15 @@ export const updateStepIndex = (step, value, row, param) => {
     return devLog.warn(`Step string missing, can't update step index!`, step) || step
   
   const { index, value:current } = row
-  const valueUpdate = checkInputValue(value, current, param)
-
-  // TODO: fix spacers for inputs
-  // Because we are splitting on space
-  // It's causing issues updating the value
-  // Need to figure out better way to handle this
-
-  const stepStr = step.step
-  const replaced = stepStr.split(' ')
-    .map((word, i) => i === index ? valueUpdate : word)
-    .join(' ')
-
-  const updated = step.step !== replaced
+  const quoted = checkInputValue(value, current, param)
+  const replaced = step.step.replace(current, quoted)
 
   return {
     ...step,
     step: replaced,
     dynamicMap: {
       ...step?.dynamicMap,
-      ...(step.step !== replaced && { [index]: valueUpdate })
+      ...(step.step !== replaced && { [index]: quoted })
     }
   }
 }
