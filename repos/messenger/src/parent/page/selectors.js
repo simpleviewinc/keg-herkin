6450cf2250cde764c39fac9f64509c334575d5a9
lancetipton <lancetipton04@gmail.com>
2021-01-04 14:26:51 -0700

    feat: Added emulate dom node helper   * Cleaned up parent selector code   * Added an ID to test an elment

--- repos/messenger/src/parent/page/selectors.js
+++ repos/messenger/src/parent/page/selectors.js
@@ -1,53 +1,13 @@
 import { exists, isFunc, get, set, ensureArr } from '@keg-hub/jsutils'
-
-const addToGlobal = (selector, elements) => {
-  elements = Array.isArray(elements) ? elements : [elements]
-  
-  const winTop = window.top
-  winTop.__MESSENGER__ = winTop.__MESSENGER__ || {}
-
-  const cachedEls = get(winTop, `__MESSENGER__.elements`, {})
-
-  cachedEls[selector]
-    ? cachedEls[selector].push(elements)
-    : (cachedEls[selector] = elements)
-  
-  set(winTop, `__MESSENGER__.elements`, cachedEls)
-}
-
-const emulateDomNode = node => {
-
-    const emulatedNode = {
-      nodeName: node.nodeName,
-      nodeType: node.nodeType,
-      tagName: node.tagName,
-      childNodes: [...node.childNodes].map(child => emulateDomNode(child)),
-      textContent: node.textContent,
-      attributes: []
-    }
-
-    node.attributes &&
-      Array.from([...node.attributes])
-        .map(attr => emulatedNode.attributes.push({ name: attr.name, value: attr.value }))
-
-    return emulatedNode
-}
+import { emulateDomNode } from '../../utils/emulateDomNode'
 
 export const $ = (selector, context=document) => {
-  // return context.querySelector(selector)
-  const element = context.querySelector(selector)
-  addToGlobal(selector, element)
-  return element
-  // return element && emulateDomNode(element)
+  return selector && emulateDomNode(context.querySelector(selector))
 }
 
 export const $$ = (selector, context=document) => {
-  return Array.from(context.querySelectorAll(selector))
-    .map(element => {
-      // emulateDomNode(element)
-      addToGlobal(element)
-      return element
-    })
+  return selector && Array.from(context.querySelectorAll(selector))
+    .map(element => emulateDomNode(element))
 }
 
 export const $eval = (selector, pageFunction, arg) => {
