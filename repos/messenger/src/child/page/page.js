b7b987d0d8105ec729f0b221532fd06b931d27cc
lancetipton <lancetipton04@gmail.com>
2021-01-05 03:08:19 -0700

    feat: Clean up   * Cleaned up package.json scripts   * Added some JSDoc notes

--- repos/messenger/src/child/page/page.js
+++ repos/messenger/src/child/page/page.js
@@ -16,6 +16,15 @@ export class Page {
   }
 
 
+  /**
+  * Selector method that wraps the parents page#selector method
+  * @function
+  * @private
+  * @param {string} selector - CSS selector for finding a dom element
+  * @param {Object|string} context - Root element search from
+  *
+  * @return {Object} - DOM node rebuilt from the reponse.outerHTML string of the Parent
+  */
   $ = async (selector, context) => {
     const response = await this.parentPage.$(
       selector,
@@ -25,6 +34,15 @@ export class Page {
     return stringToDom(response)
   }
 
+  /**
+  * Selector method for multiple elements that wraps the parents page#group_selector method
+  * @function
+  * @private
+  * @param {string} selector - CSS selector for finding a dom elements
+  * @param {Object|string} context - Root element search from
+  *
+  * @return {Object} - DOM nodes rebuilt from the response.outerHTML string of the Parent
+  */
   $$ = async (selector, context) => {
     const response = await this.parentPage.$$(
       selector,
@@ -34,6 +52,16 @@ export class Page {
     return response.map(stringToDom)
   }
 
+  /**
+  * Eval method that wraps the parents page#eval method
+  * @function
+  * @private
+  * @param {string} selector - CSS selector for finding a dom elements
+  * @param {string|function} pageFunction - Method to run on the parent
+  * @param {*} arg - Extra argument to pass to the parent
+  *
+  * @return {*} - Response from the pageFunction evaluated within the Parent Page
+  */
   $eval = async (selector, pageFunction, arg) => {
     const response = await this.parentPage.$eval(
       selector,
