189f0670cd2b588367201aa17de8abfa3d75e6ab
lancetipton <lancetipton04@gmail.com>
2021-01-12 02:41:21 -0700

    feat: Started backend files API

--- repos/backend/endpoints/index.js
+++ repos/backend/endpoints/index.js
@@ -3,11 +3,14 @@ const featuresApi = require('./features')
 const rootApi = require('./root')
 const definitionsApi = require('./definitions')
 const bddApi = require('./bdd')
+const filesApi = require('./files')
+
 
 module.exports = (app, config) => {
   bddApi(app, config)
   featuresApi(app, config)
   definitionsApi(app, config)
   componentsApi(app, config)
+  filesApi(app, config)
   rootApi(app, config)
 }
---
+++ repos/backend/libs/fileSys/testFiles.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const { readFile, removeFile, writeFile } = require('./fileSys')
+
+const deleteTestFile = async (config, testPath) => {
+  const { testsFolder } = config.editor
+  const fullPath = path.join(testsFolder, testPath)
+
+  // TODO: double check that removeFile returns a value
+  const deleted = await removeFile(fullPath)
+
+  return {
+    fullPath,
+    testPath,
+    success: Boolean(deleted),
+  }
+}
+
+const getTestFile = async (config, testPath) => {
+  const { testsFolder } = config.editor
+  const fullPath = path.join(testsFolder, testPath)
+  const content = await readFile(fullPath)
+  
+  return {
+    content,
+    fullPath,
+    testPath,
+  } 
+}
+
+const saveTestFile = async (config, testPath, content) => {
+  const { testsFolder } = config.editor
+  const fullPath = path.join(testsFolder, testPath)
+
+  // TODO: double check that writeFile returns a value
+  const saved = await writeFile(fullPath, content)
+
+  return {
+    fullPath,
+    testPath,
+    success: Boolean(saved),
+  }
+}
+
+module.exports = {
+  deleteTestFile,
+  getTestFile,
+  saveTestFile
+}
