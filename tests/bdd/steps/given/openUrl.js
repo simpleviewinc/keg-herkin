8986556dff9ca4b1710445f67284c3eee14e9ec0
mpcarolin <mpcarolin.dev@gmail.com>
2021-02-04 12:25:39 -0700

    updating cucumber-jest to use my fork

--- tests/bdd/steps/given/openUrl.js
+++ tests/bdd/steps/given/openUrl.js
@@ -2,7 +2,7 @@ const { Given } = require("cucumber")
 const { getBrowserContext } = require('../../support/setup')
 const { getPage } = getBrowserContext()
 
-Given(/I open the site (\S+)$/, async site => {
+Given('I open the site {word}', async site => {
   const page = await getPage()
   await page.goto(site)
 })
