d971e7cbde07d2db2733f0963cab314e81b661ce
lancetipton <lancetipton04@gmail.com>
2021-02-09 17:36:47 -0700

    feat(runner): Styles clean up and split components

--- repos/tap/theme/components/aceEditor/themeOverrides.js
+++ repos/tap/theme/components/aceEditor/themeOverrides.js
@@ -2,9 +2,13 @@ import { tapColors } from '../../tapColors'
 
 export const aceThemeOverrides = theme => ({
   'ace-chrome': {
-    paddingBottom: theme.padding.size
+    paddingBottom: theme.padding.size * 2
   },
   'ace-chrome .ace_gutter': {
+    paddingTop: theme.padding.size,
     background: tapColors.accentBackground,
   },
+  'ace-chrome  .ace_scroller': {
+    paddingTop: theme.padding.size,
+  }
 })
