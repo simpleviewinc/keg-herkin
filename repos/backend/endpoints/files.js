a9045bef0aeee9ca679634155ae01be9b9d5ea01
Daniel Yo <dnl992004@yahoo.com.au>
2021-02-11 12:38:09 -0700

    update: - added new method in backend api to get the fileTree array - migrated getFolderContentSync from keg-cli

--- repos/backend/endpoints/files.js
+++ repos/backend/endpoints/files.js
@@ -2,8 +2,10 @@ const { apiErr, apiResponse } = require('./handler')
 const {
   deleteTestFile,
   getTestFile,
-  saveTestFile
-} = require('../libs/fileSys/testFiles')
+  saveTestFile,
+  getFolderContentSync
+} = require('../libs/fileSys')
+const fs = require('fs')
 
 
 const saveFile = (app, config) => async (req, res) => {
@@ -34,7 +36,7 @@ const loadFile = (app, config) => async (req, res) => {
 const deleteFile = (app, config) => async (req, res) => {
   try {
     const file = req.params.file
-    const meta = await testFiles.deleteTestFile(config, file)
+    const meta = await deleteTestFile(config, file)
 
     return apiResponse(req, res, meta || {}, 200)
   }
@@ -43,7 +45,92 @@ const deleteFile = (app, config) => async (req, res) => {
   }
 }
 
+/**
+ * Recursively checks to find the parent node for a given item
+ * if a parent exists, it will add it as a child
+ * @param {Array<Object>} nodes 
+ * @param {string} parentPath 
+ * @param {Object} newItem
+ * 
+ * @returns {Boolean} - whether a parent node exists and push was successful
+ */
+const parentNodeExists = (nodes, parentPath, newItem) => {
+  
+  const found = nodes.find((node) => {
+    if (node.fullPath === parentPath) {
+      node.children.push(newItem)
+      return true
+    } 
+    else {
+      // check children
+      return node.children.length > 0 && parentNodeExists(node.children, parentPath, newItem)
+    }
+  })
+
+  return Boolean(found)
+}
+
+/**
+ * Transforms the paths string to a specific data object
+ * @param {Array<string>} paths - full paths to the folder or file i.e '/keg/tap/tests/bdd/features'
+ * 
+ * @returns {Array<Object>} - each object has the form: 
+ *                            {id, fullPath, children: []}
+ */
+const generateTree = (paths) => {
+
+  /**
+   * 1. create new object for each 'path' item
+   * 2. if the parent path of current 'path' item exists, add it as the child
+   */
+  return paths.reduce((nodes, path) => {
+    const node = {
+      id: path,
+      fullPath: path,
+      children: []
+    }
+
+    const isDir = fs.lstatSync(path).isDirectory()
+    node.type = isDir ? 'folder' : 'file'
+
+    const lastIndex = path.lastIndexOf('/')
+    const parentPath = path.substring(0, lastIndex)
+
+    // get name after last '/' excluding '/'
+    node.name = path.substring(lastIndex + 1)
+
+    // either add new node to node.children, or as a new top level node
+    nodes.length > 0
+      ? !parentNodeExists(nodes, parentPath, node) && nodes.push(node)
+      : nodes.push(node)
+    
+    return nodes
+  }, [])
+
+}
+
+/**
+ * iterates through the mounted test root folder and returns a tree like structure of all the folders/files
+ * @param {Object} app 
+ * @param {object} config - shared config
+ */
+const getTree = (app, config) => async (req, res) => {
+  try {
+    const { testsRoot } = config.paths
+    const meta = await getFolderContentSync(testsRoot, {
+      full: true,
+      recursive: true,
+    })
+    const dataTree = generateTree(meta)
+    return apiResponse(req, res, dataTree || {}, 200)
+  }
+  catch(err){
+    return apiErr(req, res, err, 400)
+  }
+}
+
 module.exports = (app, config) => {
+  app.get('/files/get_tree', getTree(app, config))
   app.get('/files/load', loadFile(app, config))
   app.post('/files/save', saveFile(app, config))
   app.delete('/files/delete', deleteFile(app, config))
--- repos/backend/libs/fileSys/fileSys.js
+++ repos/backend/libs/fileSys/fileSys.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
-const { limbo } = require('@keg-hub/jsutils')
+const path = require('path')
+const { limbo, isFunc } = require('@keg-hub/jsutils')
 
 /**
  * Wraps a method with a callback into a promise
@@ -92,6 +93,73 @@ const writeFile = (filePath, data, format='utf8') => {
 }
 
 
+/**
+ * Gets the content of a folder based on passed in options
+ * @function
+ * @param {string} fromPath - Path to get the content from
+ * @param {Array} allFound  - Past found file paths
+ * @param {string} file - File path to check if should be added to allFound array
+ * @param {boolean} opts.full - Should return the full path
+ * @param {string} opts.type - Type of content to return (folder || file)
+ * @param {Array} opts.exclude - File or folder to exclude
+ * @param {Array} opts.include - File or folder to include
+ *
+ * @returns {Array} - Array of found file paths
+ */
+const buildFoundArray = ({ allFound, recurCall, file, fromPath, opts={} }) => {
+  
+  const { exclude=[], full, include=[], recursive, type } = opts
+
+  // Filter out any folder matching the exclude
+  if(!file || exclude.indexOf(file) !== -1) return allFound
+
+  // Get the full path of the file or folder
+  const fullPath = path.join(fromPath, file)
+
+  // Check if we should use the full path or relative
+  const found = full ? fullPath : file
+
+  // Check if its a directory
+  const isDir = fs.statSync(fullPath).isDirectory()
+
+  // Check if found should be added to the array based on the passed in arguments
+  // Check the for type match or no type
+  ;( !type ||
+    ( type === 'folder' && isDir ) ||
+    ( type !== 'folder' && !isDir )) &&
+    ( !include.length || include.indexOf(file) !== -1 ) &&
+    allFound.push(found)
+
+  return !isDir || !recursive || !isFunc(recurCall)
+    ? allFound
+    : recurCall(fullPath, opts, allFound)
+
+}
+
+
+/**
+ * Gets the content of a folder based on passed in options synchronously
+ * @function
+ * @param {string} fromPath - Path to get the content from
+ * @param {Object} [opts={}] - Options for filtering the found contnet
+ * @param {boolean} opts.full - Should return the full path
+ * @param {string} opts.type - Type of content to return (folder || file)
+ * @param {Array} opts.exclude - File or folder to exclude
+ * @param {Array} opts.include - File or folder to include
+ *
+ * @returns {Promise|Array} - Array of found items
+ */
+const getFolderContentSync = (fromPath, opts={}, foundPaths=[]) => {
+  return fs.readdirSync(fromPath)
+    .reduce((allFound, file) => buildFoundArray({
+      opts,
+      file,
+      fromPath,
+      allFound,
+      recurCall: getFolderContentSync,
+    }), foundPaths)
+}
+
 module.exports = {
   copyFile,
   mkDir,
@@ -100,4 +168,5 @@ module.exports = {
   readFile,
   removeFile,
   writeFile,
+  getFolderContentSync
 }
