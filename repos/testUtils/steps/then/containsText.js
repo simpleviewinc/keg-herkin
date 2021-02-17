482d9758ea734931e37deea819b6da26638e08cd
mpcarolin <mpcarolin.dev@gmail.com>
2021-02-17 14:47:26 -0700

    updated step def

--- repos/testUtils/steps/then/containsText.js
+++ repos/testUtils/steps/then/containsText.js
@@ -2,7 +2,7 @@ const { Then } = require("cucumber")
 const { getBrowserContext } = require('../../support/setup')
 const { getPage } = getBrowserContext()
 
-Then(/the element (.*) contains the text (.*)$/, async (selector, data) => {
+Then('the element {word} contains the text {word}', async (selector, data) => {
   const page = await getPage()
   const content = await page.$eval(selector, el => el.textContent)
   expect(content).toEqual(expect.stringContaining(data))
