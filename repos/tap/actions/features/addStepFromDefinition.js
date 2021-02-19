cd83350f80a7d3125cda0848c6ce17d63b73e33d
lancetipton <lancetipton04@gmail.com>
2021-02-19 15:46:59 -0700

    feat(clipboard): Added copy to clip board * Allows copying a defs matcher text to clipboard

--- repos/tap/actions/features/addStepFromDefinition.js
+++ repos/tap/actions/features/addStepFromDefinition.js
@@ -2,49 +2,10 @@ import { dispatch } from 'SVStore'
 import { loadFeature } from './loadFeature'
 import { uuid, noOpObj } from '@keg-hub/jsutils'
 import { Values, ActionTypes } from 'SVConstants'
+import { copyToDefinitionClipboard } from 'SVUtils/definitions'
 import { getActiveFeature, validateFeatureAction } from 'SVUtils/features'
 
 const { CATEGORIES } = Values
-
-const noScenarioFound = (context, feature) => {
-  return console.warn(
-    `The feature scenario could not be found from the context`,
-    { context, feature },
-  )
-}
-
-const getLastScenario = feature => {
-  return feature &&
-    feature.scenarios &&
-    feature.scenarios.length &&
-    feature.scenarios[feature.scenarios.length - 1]
-}
-
-const getScenarioFromContext = (context, feature) => {
-  // May want to add other context for adding a step to a features scenario
-  // For now just check if we have access to the features AceEditor object
-  if(!context.editor) return
-
-  const editor = context.editor
-  const { row, column } = editor.getCursorPosition()
-
-  if(!row && !column) return getLastScenario(feature)
-
-}
-
-const stepFactory = (definition=noOpObj) => {
-  return {
-    uuid: uuid(),
-    step: definition.name,
-    type: definition.type,
-    definition: definition.uuid,
-    dynamicMap: definition?.tokens?.reduce((mapped, token) => {
-      token.dynamic && (mapped[token.index] = token.value)
-
-      return mapped
-    }, {}),
-  }
-}
 
 /**
  * Adds a step from the definition to currently active feature
@@ -57,10 +18,11 @@ const stepFactory = (definition=noOpObj) => {
  *
  * @return {Object} - Found active feature
  */
-export const addStepFromDefinition = (definition, context, feature=getActiveFeature()) => {
-  const scenario = getScenarioFromContext(context, feature)
-  if(!scenario) return noScenarioFound(context, feature)
+export const addStepFromDefinition = (args) => {
+  const { definition, clipboard } = args
 
-  const step = stepFactory(definition)
+  if(clipboard) return copyToDefinitionClipboard(definition)
+
+  // TODO: Add other options for auto setting the step definition into the feature 
 
 }
--- repos/tap/components/definition/definitionList.js
+++ repos/tap/components/definition/definitionList.js
@@ -4,11 +4,19 @@ import { reduceObj } from '@keg-hub/jsutils'
 import { useStyle } from '@keg-hub/re-theme'
 import React, { useMemo, useCallback } from 'react'
 import { useSelector, shallowEqual } from 'react-redux'
-import { ChevronDown, PlusCircle } from 'SVAssets/icons'
+import { ChevronDown, Copy } from 'SVAssets/icons'
 import { SimpleList, Row, Text, View, Touchable } from 'SVComponents'
 import { addStepFromDefinition } from 'SVActions/features/addStepFromDefinition'
 const { CATEGORIES, EDITOR_TABS } = Values
 
+/**
+ * Maps the definitions to a format that can be loaded by the SimpleList Component
+ * Separates them by type, and creates a lookup map
+ * @private
+ * @param {Object} definitions - Groups of step definitions to search separated by type
+ *
+ * @return {Object} - Mapped definitions into SimpleList formatted object
+ */
 const useDefinitionGroups = definitions => {
   return useMemo(() => {
     return reduceObj(definitions, (key, defs, grouped) => {
@@ -19,12 +27,12 @@ const useDefinitionGroups = definitions => {
           title: `${def.type} ${def.name}`,
           uuid: def.uuid,
           actions: [{
-            name: 'add',
-            key: `action-add`,
+            name: 'Copy to Clipboard',
+            key: `action-copy`,
             showFeedback: false,
             iconProps: {
               size: 14,
-              Component: PlusCircle,
+              Component: Copy,
             },
           }]
         }
@@ -44,11 +52,18 @@ export const DefinitionList = props => {
   const { lookup, ...groupedDefs } = useDefinitionGroups(definitions)
 
   const onItemPress = useCallback((item, event) => {
+    // TODO: feature and context are not currently used
+    // Right now it just copies the text to the clipboard
     // Lookup the definition from the lookup table using the uuid
     // Then add it to the active feature by calling the addStepFromDefinition action
     const definition = lookup[item.uuid]
     definition
-      ? addStepFromDefinition(definition, contextRef.current, feature)
+      ? addStepFromDefinition({
+          feature,
+          definition,
+          clipboard: true,
+          context: contextRef.current,
+        })
       : console.warn(`Could not find matching definition for item:`, item)
   }, [lookup, feature, contextRef.current])
 
