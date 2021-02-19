0836a530f3b481c520b20bcbb9720c138d4d92a2
mpcarolin <mpcarolin.dev@gmail.com>
2021-02-19 09:21:36 -0700

    fixed afterAll

--- repos/testUtils/parkin/hooks.js
+++ repos/testUtils/parkin/hooks.js
@@ -2,5 +2,5 @@ const { parkin } = require('./instance')
 
 module.exports = {
   BeforeAll: parkin.hooks.beforeAll.bind(parkin.hooks),
-  AfterAll: parkin.hooks.beforeAll.bind(parkin.hooks),
+  AfterAll: parkin.hooks.afterAll.bind(parkin.hooks),
 }
