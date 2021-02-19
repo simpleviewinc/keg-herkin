8441343e75eaee5324800e68ff3329f5904fb405
lancetipton <lancetipton04@gmail.com>
2021-02-19 15:25:31 -0700

    feat(match): Added code to match feat to defs in the editors

--- repos/tap/components/definition/definitionsEditor.js
+++ repos/tap/components/definition/definitionsEditor.js
@@ -1,5 +1,5 @@
 import { Values } from 'SVConstants'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { noOpObj } from '@keg-hub/jsutils'
 import { View } from '@keg-hub/keg-components'
 import { DefinitionList } from './definitionList'
@@ -7,12 +7,18 @@ import { DefinitionTabs } from './definitionTabs'
 import { useActiveTab } from 'SVHooks/useActiveTab'
 import { ActiveDefinitionsEditor } from './activeDefinitionsEditor'
 
-const { DEFINITION_TABS } = Values
+const { DEFINITION_TABS, STEP_TYPES } = Values
+
+const getDefinitionIdFromText = (feature, text, definitions) => {
+  const matchingStep = feature.scenarios.reduce((found, scenario) => {
+    return found || scenario.steps.find(step => step.step === text)
+  }, false)
+} 
 
 export const DefinitionsEditor = props => {
   const {
-    // activeTab=DEFINITION_TABS.ACTIVE,
-    activeTab=DEFINITION_TABS.LIST,
+    activeTab=DEFINITION_TABS.ACTIVE,
+    // activeTab=DEFINITION_TABS.LIST,
     active,
     feature,
     featureEditorRef,
@@ -22,6 +28,26 @@ export const DefinitionsEditor = props => {
   } = props
 
   const [tab, setTab] = useActiveTab(activeTab)
+
+  useEffect(() => {
+    if(tab === DEFINITION_TABS.LIST) return
+
+    const editor = featureEditorRef.current.editor
+    editor.session.selection.on('changeCursor', () => {
+      // const cursorPos = editor.getCursorPosition()
+      const lineContent = editor.session.getLine(editor.getSelectionRange().start.row)
+      const [ type, ...content ] = lineContent.trim().split(' ')
+
+      getDefinitionIdFromText(feature, content.join(' '), active)
+
+    })
+
+  }, [
+    tab,
+    active,
+    feature,
+    featureEditorRef.current
+  ])
 
   return (
     <View
