3a40918e188ea39863c648625199b4202da20005
mpcarolin <mpcarolin.dev@gmail.com>
2021-02-18 17:44:54 -0700

    setup parkin sync; setup hooks

--- repos/testUtils/parkin/hooks.js
+++ repos/testUtils/parkin/hooks.js
@@ -1,22 +1,6 @@
-const { isFunc, validate } = require('@keg-hub/jsutils')
-
-const clientHooks = {
-  BeforeAll: () => {},
-  AfterAll: () => {}
-}
-
-const AfterAll = afterAllFn => {
-  validate({ afterAllFn }, { afterAllFn: isFunc }, { throws: true })
-  clientHooks.AfterAll = afterAllFn
-}
-
-const BeforeAll = beforeAllFn => {
-  validate({ beforeAllFn }, { beforeAllFn: isFunc }, { throws: true })
-  clientHooks.BeforeAll = beforeAllFn
-}
+const { parkin } = require('./instance')
 
 module.exports = {
-  BeforeAll,
-  AfterAll,
-  clientHooks
+  BeforeAll: parkin.hooks.beforeAll.bind(parkin.hooks),
+  AfterAll: parkin.hooks.beforeAll.bind(parkin.hooks),
 }
--- repos/testUtils/parkin/runFeatures.js
+++ repos/testUtils/parkin/runFeatures.js
@@ -1,19 +1,12 @@
 const { loadFeatures } = require('./loadFeatures')
 const { parkin } = require('./instance')
-const { clientHooks } = require('./hooks')
 
 /**
  * Run this inside a jest process, as it creates `describe` blocks
  */
-const runFeatures = () => {
-  try {
-    clientHooks.BeforeAll()
-    const features = loadFeatures()
-    parkin.run(features)
-  }
-  finally {
-    clientHooks.AfterAll()
-  }
+const runFeatures = async () => {
+  const features = loadFeatures()
+  parkin.run(features)
 }
 
 module.exports = { runFeatures }
