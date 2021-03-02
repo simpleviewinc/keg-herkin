fa0204290ec6eac33dd71bb5e6574ecf4dd4159a
lancetipton <lancetipton04@gmail.com>
2021-03-02 02:14:28 -0700

    fix(cleanup): Cleaned up the init action

--- repos/tap/actions/init.js
+++ repos/tap/actions/init.js
@@ -1,48 +1,33 @@
-import { setScreen } from './setScreen'
 import { Values } from 'SVConstants'
-import { apiRequest } from 'SVUtils/api/apiRequest'
-import { loadApiFile } from 'SVUtils/api/loadApiFile'
-import { upsertFeatures }  from './features/upsertFeatures'
-import { setFeatureActive } from './features/setFeatureActive'
-import { queryToObj, noOpObj, isEmptyColl } from '@keg-hub/jsutils'
-import { upsertDefinitions }  from './definitions/upsertDefinitions'
-import { upsertActiveRunnerTest }  from './runner/upsertActiveRunnerTest'
-import { setActiveModal } from 'SVActions/modals'
-import { setActiveSidebar } from 'SVActions/sidebar'
+import { setScreen } from 'SVActions/screens'
+import { isEmptyColl } from '@keg-hub/jsutils'
 import { upsertFileTree } from 'SVActions/files'
+import { setActiveModal } from 'SVActions/modals'
+import { apiRequest } from 'SVUtils/api/apiRequest'
+import { setActiveSidebar } from 'SVActions/sidebar'
+import { getQueryData } from 'SVUtils/helpers/getQueryData'
+
 const { MODAL_TYPES, SIDEBAR_TYPES } = Values
 
-const getQueryData = () => {
-  return typeof document === 'undefined'
-    ? noOpObj
-    : queryToObj(document?.location?.search)
+/**
+ * Checks if an initial screen should be set, and makes call to set it
+ * @function
+ * @param {Object} queryObj - Current url query params as an object
+ *
+ * @return {void}
+ */
+const loadInitScreen = async queryObj => {
+  setScreen(queryObj.tab || 'empty')
 }
 
-const cleanFeatName = feat => feat.toLowerCase().replace(/ /g, '')
-
-const initFeatures = (features, queryFeat) => {
-  if(!features || !features.length) return
-
-  // Add all features to the store
-  upsertFeatures(features)
-
-  // Check for an active feature from the current url query object
-  const activeFeat = queryFeat &&
-    features &&
-    features.length &&
-    features.find(feat => cleanFeatName(feat.feature) === cleanFeatName(queryFeat))
-
-  activeFeat && setFeatureActive(activeFeat)
-  
-  return activeFeat
-}
-
-const initDefs = definitions => {
-  // Add all definitions to the store
-  definitions && upsertDefinitions(definitions)
-}
-
-const initTestFile = async (activeFeat, queryFile) => {
+/**
+ * Checks if an initial test file should be loaded, and makes call to load it
+ * @function
+ * @param {Object} queryObj - Current url query params as an object
+ *
+ * @return {void}
+ */
+const loadInitTestFile = async queryObj => {
   if(activeFeat && activeFeat.content)
     return upsertActiveRunnerTest(activeFeat)
 
@@ -52,6 +37,18 @@ const initTestFile = async (activeFeat, queryFile) => {
   upsertFileTree(await apiRequest(`/files/tree`) || {})
 }
 
+/**
+ * Checks if the initial settings modal should be shown, and makes call to update the store 
+ * @function
+ * @param {Object} queryObj - Current url query params as an object
+ *
+ * @return {void}
+ */
+const loadInitModal = async queryObj => {
+  // display options modal if no valid querystring passed in
+  ;(!queryObj || isEmptyColl(queryObj)) &&
+    setActiveModal(MODAL_TYPES.TEST_SELECTOR_MODAL)
+}
 
 /**
  * Init action
@@ -59,21 +56,18 @@ const initTestFile = async (activeFeat, queryFile) => {
  * @function
  */
 export const init = async () => {
-  const { features, definitions } = await apiRequest(`/bdd`)
+  // Get the query params from the url
   const queryObj = getQueryData()
 
-  const activeFeat = initFeatures(features, queryObj.feature)
+  // Load the initial test file
+  await loadInitTestFile(queryObj)
 
-  initDefs(definitions)
+  // Load the initial screen
+  await loadInitScreen(queryObj)
 
-  initTestFile(activeFeat, queryObj?.file)
-
+  // Setup the sidebar
   setActiveSidebar(SIDEBAR_TYPES.FILE_TREE)
-  // display options modal if no valid querystring passed in
-  ;(!queryObj || isEmptyColl(queryObj)) &&
-    setActiveModal(MODAL_TYPES.TEST_SELECTOR_MODAL)
 
-  // Update the current screen to match the query.tab value
-  setScreen(queryObj.tab || 'empty')
-
+  // Load the init modal
+  loadInitModal(queryObj)
 }
