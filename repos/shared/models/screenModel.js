8edd7bbee292666fb3323c5317bc82492fdb282e
lancetipton <lancetipton04@gmail.com>
2021-03-02 01:16:58 -0700

    feat(models): Added more models and a buildModel helper

---
+++ repos/shared/models/screenModel.js
@@ -0,0 +1,20 @@
+const { buildModel } = require('./buildModel')
+
+/**
+ * Models a UI screen
+ * @typedef ScreenModel
+ * @property {string} file - Uuid of the active file of the screen
+ * @property {boolean} isActive - Is the screen active in the UI
+ * @property {name} name - Name of the screen
+ */
+const Model = {
+  file: ,
+  isActive: false,
+  name: '',
+}
+
+const testsModel = buildModel(overrides, Model)
+
+module.exports = {
+  testsModel
+}
