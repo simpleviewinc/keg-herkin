a57b6d8a2943e05bcfc30a3bcbf8d39a40e0ab99
lancetipton <lancetipton04@gmail.com>
2021-03-09 02:37:02 -0700

    feat(parkin): Added some notes about parkin run options

--- repos/testUtils/parkin/setupTestEnvironment.js
+++ repos/testUtils/parkin/setupTestEnvironment.js
@@ -1,4 +1,15 @@
 const { parkin } = require('./instance')
+const { getHerkinConfig } = require('HerkinConfigs/getHerkinConfig')
 
+/**
+ * Global helper to allow re-using the same parking instance for each test
+ */
 global.getParkinInstance = () => parkin
+
+
+/**
+ * TODO: build dynamic options based on the current env
+ * Most likely will need to use process.env to pass in options
+ * Need a way to get the name, and tags options passed here
+ */
 global.getParkinOptions = () => ({})
