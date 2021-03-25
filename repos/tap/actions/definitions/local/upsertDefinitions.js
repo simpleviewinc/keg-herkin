9ea287958ee6e6d04667e5b9f934ef386f3b0100
Lance Tipton <lancetipton04@gmail.com>
2021-03-24 18:21:49 -0700

    fix(PR): Added PR fixes

--- repos/tap/actions/definitions/local/upsertDefinitions.js
+++ repos/tap/actions/definitions/local/upsertDefinitions.js
@@ -1,5 +1,6 @@
 import { devLog } from 'SVUtils'
 import { dispatch } from 'SVStore'
+import { upsertItems } from 'SVActions'
 import { noOpObj } from '@keg-hub/jsutils'
 import { Values, ActionTypes } from 'SVConstants'
 import { definitionsByType } from 'SVUtils/shared'
@@ -14,24 +15,12 @@ const { CATEGORIES } = Values
  * @returns {void}
  */
 export const upsertDefinitions = (definitions, definitionTypes) => {
-  definitions &&
-    dispatch({
-      type: ActionTypes.UPSERT_ITEMS,
-      payload: {
-        category: CATEGORIES.DEFINITIONS,
-        items: definitions,
-      },
-    })
-
+  definitions && upsertItems(CATEGORIES.DEFINITIONS, definitions)
   // Sort the definitions by type ( given | then | etc... )
   // This makes it easier to match to features when editing
-  definitionTypes &&
-    dispatch({
-      type: ActionTypes.UPSERT_ITEMS,
-      payload: {
-        category: CATEGORIES.DEFINITION_TYPES,
-        items: definitionTypes || definitionsByType(definitions),
-      },
-    })
-
+  ;(definitions || definitionTypes) &&
+    upsertItems(
+      CATEGORIES.DEFINITION_TYPES,
+      definitionTypes || definitionsByType(definitions)
+    )
 }
