9b1828c4017d3e43b4934c8404d1af10a326aae7
Lance Tipton <lancetipton04@gmail.com>
2021-04-22 15:05:24 -0700

    feat(container): Bundle Automation       * Updated container files to allow serving the bundled app     * Added new DOC_BUILD_PATH env

--- container/run.sh
+++ container/run.sh
@@ -1,33 +1,30 @@
 #!/usr/bin/env
 
+# Ensure required envs are set
+[[ -z "$KEG_PROXY_PORT" ]] && KEG_PROXY_PORT=19006
+[[ -z "$DOC_APP_PATH" ]] && DOC_APP_PATH=/keg/tap
+[[ -z "$DOC_BUILD_PATH" ]] && DOC_BUILD_PATH=/keg/tap-build
 
 # If the no KEG_DOCKER_EXEC env is set, just sleep forever
 # This is to keep our container running forever
-if [ -z "$KEG_DOCKER_EXEC" ]; then
-  tail -f /dev/null
-  exit 0
+[[ -z "$KEG_DOCKER_EXEC" ]] && tail -f /dev/null && exit 0;
 
-else
-
-  # Ensure the DOC_APP_PATH is set
-  if [ -z "$DOC_APP_PATH" ]; then
-    DOC_APP_PATH=/keg/tap
-  fi
-
-  # cd into the tap repo
+# Serve the bundle and also run the backend api
+keg_herkin_serve(){
   cd $DOC_APP_PATH
+  npx serve $DOC_BUILD_PATH --cors -n -l $KEG_PROXY_PORT &>/dev/null & disown;
+  node ./repos/backend/index.js
+  exit 0
+}
 
-  # Check if no exec command exists, or if it's set to web
-  # Then default it to run the re-theme app
-  if [ -z "$KEG_EXEC_CMD" ] || [ "$KEG_EXEC_CMD" == 'web' ] || [ "$KEG_EXEC_CMD" == 'start' ]; then
-    KEG_EXEC_CMD="dev"
-  fi
-
-  # Start the tap instance
-  echo $"[ KEG-CLI ] Running command 'yarn $KEG_EXEC_CMD'" >&2
-  yarn $KEG_EXEC_CMD
-
+# Check the NODE_ENV, and use that to know which environment to start
+# For non-development environments, we want to serve the bundle if it exists
+if [[ ! " development develop local test " =~ " $NODE_ENV " ]]; then
+  [[ -d "$DOC_BUILD_PATH" ]] && keg_herkin_serve
+  echo $"[ KEG-CLI ] Serve path $DOC_BUILD_PATH does not exist!" >&2
 fi
 
-
-
+# Serve the app bundle in development environemnts
+echo $"[ KEG-CLI ] Running development server!" >&2
+cd $DOC_APP_PATH
+[[ -z "$KEG_EXEC_CMD" ]] && yarn web || yarn $KEG_EXEC_CMD
