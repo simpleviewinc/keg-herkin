bcabc7ef53e633e33dd4996df0460b8afd3aeaea
lancetipton <lancetipton04@gmail.com>
2021-09-29 00:48:17 -0700

    fix(Def List): Fixed the definitions list to display properly   * Updated minor styles   * Refactored the simplelist   * Moved useDefinitionGroups to it's own hook file   * Removed unused code

--- repos/tap/components/definition/definitionList.js
+++ repos/tap/components/definition/definitionList.js
@@ -1,103 +1,11 @@
-import { noOpObj } from '@keg-hub/jsutils'
-import { reduceObj } from '@keg-hub/jsutils'
-import { useStyle } from '@keg-hub/re-theme'
 import React, { useMemo, useCallback, useState } from 'react'
+import { useStyle } from '@keg-hub/re-theme'
 import { ChevronDown, Copy } from 'SVAssets/icons'
+import { DefinitionListItem } from './definitionListItem'
+import { noOpObj, reduceObj, deepClone } from '@keg-hub/jsutils'
+import { useDefinitionGroups } from 'SVHooks/useDefinitionGroups'
 import { SimpleList, Row, Text, View, Touchable } from 'SVComponents'
 import { addStepFromDefinition } from 'SVActions/features/local/addStepFromDefinition'
-import { DefinitionListItem } from './definitionListItem'
-
-/**
- * Sorts the passed in array of items alphabetically based on each items title property
- * @private
- * @param {Array<Object>} items - Group of objects with a title property
- *
- * @return {Object} - Items sorted alphabetically
- */
-const alphaSort = items => {
-  items.sort(function(a, b) {
-    const textA = a.title.toLowerCase()
-    const textB = b.title.toLowerCase()
-    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0
-  })
-
-  return items
-}
-
-/**
- * Sorts each definition group type alphabetically
- * Also sorts the all group by definition type
- * @private
- * @param {Object} grouped - Groups of step definitions separated by type
- *
- * @return {Object} - Sorted Groups of definitions alphabetically and by type 
- */
-const sortDefinitions = grouped => {
-
-  grouped.all.items.sort((a, b) => {
-    const textA = a.title.toLowerCase()
-    const textB = b.title.toLowerCase()
-    const aWhen = textA.startsWith('when')
-    const bThen = textB.startsWith('then')
-    const aThen = textA.startsWith('then')
-    const bWhen = textB.startsWith('when')
-
-    if(aWhen && bThen) return -1
-    if(aThen && bWhen) return 1
-
-    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0
-  })
-
-  grouped.given.items = alphaSort(grouped.given.items)
-  grouped.when.items = alphaSort(grouped.when.items)
-  grouped.then.items = alphaSort(grouped.then.items)
-
-  return grouped
-}
-
-/**
- * Maps the definitions to a format that can be loaded by the SimpleList Component
- * Separates them by type, and creates a lookup map
- * @private
- * @param {Object} definitions - Groups of step definitions to search separated by type
- *
- * @return {Object} - Mapped definitions into SimpleList formatted object
- */
-const useDefinitionGroups = definitions => {
-  return useMemo(() => {
-    return reduceObj(definitions, (key, defs, grouped) => {
-
-      defs.map(def => {
-        const itemProps = {
-          title: `${def.type} ${def.name}`,
-          uuid: def.uuid,
-          meta: def.meta,
-          actions: [{
-            name: 'Copy',
-            key: `action-copy`,
-            iconProps: {
-              size: 12,
-              Component: Copy,
-            },
-          }]
-        }
-        grouped[key].items.push(itemProps)
-        grouped.all.items.push(itemProps)
-        grouped.lookup[def.uuid] = def
-      })
-
-      return sortDefinitions(grouped)
-    }, {
-      lookup: {},
-      all: { type: 'all', group: 'All Steps', toggled: true, items: [] },
-      given: { type: 'given', group: 'Given Steps', toggled: false, items: [] },
-      when: { type: 'when', group: 'When Steps', toggled: false, items: [] },
-      then: { type: 'then', group: 'Then Steps', toggled: false, items: [] },
-    })
-  }, [ definitions ])
-}
-
-const renderItem = props => (<DefinitionListItem {...props} />)
 
 /**
  * DefinitionList - Renders a list of Step Definitions
@@ -157,9 +65,7 @@ export const DefinitionList = props => {
       <SimpleList
         styles={listStyles.list}
         items={listItems}
-        toggled={false}
-        headerToggle={false}
-        renderItem={renderItem}
+        renderItem={DefinitionListItem}
         onHeaderPress={onHeaderPress}
         onItemPress={onItemPress}
         HeaderIcon={ChevronDown}
